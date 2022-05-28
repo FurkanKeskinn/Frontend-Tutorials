2dcc6108794538eadbe768ad86c60585711234c5
Furkan Keskin <80197323+FurkanKeskinn@users.noreply.github.com>
2022-05-28 14:11:30 +0300

    first commit

---
+++ CSS Tutorials/CSS_Tutuorilas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSS_Tutuorilas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
